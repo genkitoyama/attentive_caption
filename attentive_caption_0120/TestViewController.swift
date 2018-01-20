12b66fe8da7437cb86c8f681bd75319d6d78ca42
genkitoyama <burst.blaster@gmail.com>
2018-01-20 23:00:11 +0900

    Initial commit

---
+++ attentive_caption_0120/TestViewController.swift
@@ -0,0 +1,151 @@
+//
+//  TestViewController.swift
+//  momomind_ios11
+//
+//  Created by Kentaro Matsumae on 2017/06/25.
+//  Copyright © 2017年 kenmaz.net. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class TestViewController: UIViewController {
+    
+    lazy var momomind: How_old_0115 = {
+        let m = How_old_0115()
+        return m
+    }()
+    
+    var currentName: String = ""
+    var success = 0
+    
+    @IBAction func trainButtonDidTap(_ sender: Any) {
+        execute(type: "train")
+    }
+    
+    @IBAction func testButtonDidTap(_ sender: Any) {
+        execute(type: "test")
+    }
+    
+    @IBAction func closeButtonDidTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+  
+    func execute(type: String) {
+        let names = [
+            "0-15male",
+            "0-15female",
+            "16-29male",
+            "16-29female",
+            "30-49male",
+            "30-49female",
+            "50-male",
+            "50-female",
+        ]
+        var total = 0
+        success = 0
+        
+        for name in names {
+            print("processing..\(type) \(name)")
+            
+            currentName = name
+            let dir = "out/\(type)/\(name)"
+            let jpgs = Bundle.main.paths(forResourcesOfType: "jpg", inDirectory: dir)
+            total += jpgs.count
+            
+            for path in jpgs {
+                let image = UIImage(contentsOfFile: path)?.cgImage
+                let input = CIImage(cgImage: image!)
+                predicate(image: input)
+            }
+        }
+        let acc = Float(success) / Float(total)
+        print("success: \(success) / \(total), accu:\( acc )")
+    }
+    
+    private func predicate(image: CIImage) {
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            let model = try VNCoreMLModel(for: self.momomind.model)
+            let req = VNCoreMLRequest(model: model, completionHandler: self.handleClassification)
+            try handler.perform([req])
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func handleClassification(request: VNRequest, error: Error?) {
+        guard
+            let observations = request.results as? [VNClassificationObservation],
+            let best = observations.first else {
+            fatalError("unexpected result type from VNCoreMLRequest")
+        }
+        if currentName == best.identifier {
+            success += 1
+        }
+    }
+}
+
+
+//MARK: debug
+
+extension TestViewController {
+    
+//    func dumpRawData(imageRef: CGImage) -> [UInt8] {
+//        guard let data = imageRef.dataProvider?.data else {
+//            assertionFailure()
+//            return []
+//        }
+//        let length = CFDataGetLength(data)
+//        var rawData = [UInt8](repeating: 0, count: length)
+//        let range = CFRange(location: 0, length: length)
+//        CFDataGetBytes(data, range, &rawData)
+//        print(rawData)
+//    }
+
+    //FIXME: dosen't work
+    func _dumpRawData(from image: CGImage) {
+        
+        let options = [
+            kCVPixelBufferCGImageCompatibilityKey as String: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey as String: true
+        ]
+        var pxBuffer: CVPixelBuffer? = nil
+        let width = image.width
+        let height = image.height
+        let res = CVPixelBufferCreate(kCFAllocatorDefault,
+                                      width,
+                                      height,
+                                      kCVPixelFormatType_32ARGB,
+                                      options as CFDictionary?,
+                                      &pxBuffer)
+        guard res == kCVReturnSuccess else {
+            assertionFailure()
+            return
+        }
+        guard let buf = pxBuffer else {
+            assertionFailure()
+            return
+        }
+        let ctx = CIContext()
+        let input = CIImage(cgImage: image)
+        ctx.render(input, to: buf)
+        
+        guard CVPixelBufferLockBaseAddress(buf, CVPixelBufferLockFlags.readOnly) == kCVReturnSuccess else {
+            assertionFailure()
+            return
+        }
+        let size = CVPixelBufferGetDataSize(buf)
+        print(size)
+        guard let pointer = CVPixelBufferGetBaseAddress(buf) else {
+            assertionFailure()
+            return
+        }
+        let p = pointer.bindMemory(to: Int8.self, capacity: 128)
+        for i in 0..<128 {
+            print(p.advanced(by: i).pointee)
+        }
+        
+        CVPixelBufferUnlockBaseAddress(buf, CVPixelBufferLockFlags.readOnly)
+    }
+}
---
+++ attentive_caption_0120/ViewController.swift
@@ -0,0 +1,495 @@
+//
+//  ViewController.swift
+//  attentive_caption_0120
+//
+//  Created by 登山元気 on 2018/01/20.
+//  Copyright © 2018年 genki toyama. All rights reserved.
+//
+//
+//  ViewController.swift
+//  momomind_ios11
+//
+//  Created by Kentaro Matsumae on 2017/06/16.
+//  Copyright © 2017年 kenmaz.net. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController {
+    
+    //MARK: IBOutlet
+    @IBOutlet weak var overlayView: OverlayView!
+    @IBOutlet weak var facePreview: UIImageView!
+    
+    @IBOutlet weak var facePos: UILabel!
+    @IBOutlet weak var faceTime: UILabel!
+    
+    @IBOutlet var progressViews: [UIProgressView]!{
+        didSet {
+            progressViews.forEach {
+                $0.transform = CGAffineTransform(scaleX: 1, y: 3)
+                $0.progress = 0
+            }
+        }
+    }
+    
+    @IBOutlet var probabilityLabels: [UILabel]! {
+        didSet {
+            probabilityLabels.forEach {
+                $0.text = "-%"
+            }
+        }
+    }
+    
+    @IBAction func infoButtonDidTap(_ sender: Any) {
+        let con = UIStoryboard(name: "TestViewController", bundle: nil).instantiateInitialViewController() as! TestViewController
+        present(con, animated: true, completion: nil)
+    }
+    
+    //MARK: vraiables
+    let session = AVCaptureSession()
+    var device: AVCaptureDevice?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var connection : AVCaptureConnection?
+    
+    let inputSize: Float = 112
+    
+    static var isFrontCamera: Bool = true
+    
+    //時間計測用の変数.
+    var cnt : Float = 0
+    //タイマー.
+    var timer : Timer!
+    var currentFaceNum : Int = 0
+    var previousFaceNum : Int = 0
+    var count : Int = 0
+    
+    let momomind = How_old_0115()
+    
+    lazy var faceRequest: VNDetectFaceRectanglesRequest = {
+        return VNDetectFaceRectanglesRequest(completionHandler: self.vnRequestHandler)
+    }()
+    
+    var inputImage:CIImage?
+    var overlayViewSize: CGSize?
+    var videoDims: CMVideoDimensions?
+    
+    
+    //MARK: funcs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVideoCapture(isBack: true)
+        
+        //タイマーを作る.
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.onUpdate(timer:)), userInfo: nil, repeats: true)
+    }
+    
+    //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
+    @objc func onUpdate(timer : Timer){
+        if(currentFaceNum > 0){
+            cnt += 0.05
+        }
+        //桁数を指定して文字列を作る.
+        let str = "faceTime: ".appendingFormat("%.2f",cnt)
+        faceTime.text = str
+        
+        previousFaceNum = currentFaceNum
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        overlayViewSize = overlayView.frame.size
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer?.frame = view.layer.bounds
+    }
+}
+
+//MARK: - setup video
+
+extension ViewController {
+    
+    private var isActualDevice: Bool {
+        return TARGET_OS_SIMULATOR != 1
+    }
+    
+    private func startSession() {
+        guard isActualDevice else { return }
+        if !session.isRunning {
+            session.startRunning()
+        }
+    }
+    
+    private func stopSession() {
+        guard isActualDevice else { return }
+        if session.isRunning {
+            session.stopRunning()
+        }
+    }
+    
+    static func findDevice(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        if position == .front{
+            self.isFrontCamera = true
+        }else{
+            self.isFrontCamera = false
+        }
+        
+        return AVCaptureDevice.default( .builtInWideAngleCamera,
+                                        for: AVMediaType.video,
+                                        position: position)
+    }
+    
+    func setupVideoCapture(isBack: Bool) {
+        guard isActualDevice else { return }
+        
+        let device = ViewController.findDevice(position: .front)!
+        
+        self.device = device
+        session.sessionPreset = AVCaptureSession.Preset.inputPriority
+        device.formats.forEach { (format) in
+            print(format)
+        }
+        
+        print("format:",device.activeFormat)
+        print("min duration:", device.activeVideoMinFrameDuration)
+        print("max duration:", device.activeVideoMaxFrameDuration)
+        
+        do {
+            try device.lockForConfiguration()
+        } catch {
+            fatalError()
+        }
+        device.activeVideoMaxFrameDuration = CMTimeMake(1, 3)
+        device.activeVideoMinFrameDuration = CMTimeMake(1, 3)
+        device.unlockForConfiguration()
+        
+        let desc = device.activeFormat.formatDescription
+        self.videoDims = CMVideoFormatDescriptionGetDimensions(desc)
+        
+        // Input
+        let deviceInput: AVCaptureDeviceInput
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: device)
+        } catch {
+            fatalError()
+        }
+        guard session.canAddInput(deviceInput) else {
+            fatalError()
+        }
+        session.addInput(deviceInput)
+        
+        // Preview:
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.contentsGravity = kCAGravityResizeAspectFill
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer.insertSublayer(previewLayer, at: 0)
+        self.previewLayer = previewLayer
+        
+        // Output
+        let output = AVCaptureVideoDataOutput()
+        let key = kCVPixelBufferPixelFormatTypeKey as String
+        let val = kCVPixelFormatType_32BGRA as NSNumber
+        output.videoSettings = [key: val]
+        output.alwaysDiscardsLateVideoFrames = true
+        let queue = DispatchQueue(label: "net.kenmaz.momomind")
+        output.setSampleBufferDelegate(self, queue: queue)
+        guard session.canAddOutput(output) else {
+            fatalError()
+        }
+        session.addOutput(output)
+        
+        self.connection = output.connection(with: AVMediaType.video)
+    }
+}
+
+//MARK: - Video Capture
+
+//reference: https://qiita.com/marty-suzuki/items/496f211e22cad1f8de19
+//extension DispatchQueue {
+//    func throttle(delay: DispatchTimeInterval) -> (_ action: @escaping () -> ()) -> () {
+//        var lastFireTime: DispatchTime = .now()
+//
+//        return { [weak self, delay] action in
+//            let deadline: DispatchTime = .now() + delay
+//            self?.asyncAfter(deadline: deadline) { [delay] in
+//                let now: DispatchTime = .now()
+//                let when: DispatchTime = lastFireTime + delay
+//                if now < when { return }
+//                lastFireTime = .now()
+//                action()
+//            }
+//        }
+//    }
+//
+//    func debounce (delay: DispatchTimeInterval) -> (_ action: @escaping () -> ()) -> () {
+//        var lastFireTime: DispatchTime = .now()
+//
+//        return { [weak self, delay] action in
+//            let deadline: DispatchTime = .now() + delay
+//            lastFireTime = .now()
+//            self?.asyncAfter(deadline: deadline) { [delay] in
+//                let now: DispatchTime = .now()
+//                let when: DispatchTime = lastFireTime + delay
+//                if now < when { return }
+//                lastFireTime = .now()
+//                action()
+//            }
+//        }
+//    }
+//}
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    // 毎フレーム実行される処理
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if connection.videoOrientation != .portrait {
+            connection.videoOrientation = .portrait
+            return
+        }
+        if let buffer = CMSampleBufferGetImageBuffer(sampleBuffer), connection == self.connection {
+            let timestamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+            processBuffer(timestamp, buffer)
+        }
+    }
+    
+    private func processBuffer(_ timestamp: CMTime, _ buffer: CVImageBuffer) {
+        let inputImage = CIImage(cvImageBuffer: buffer)
+        let handler = VNImageRequestHandler(ciImage: inputImage)
+        self.inputImage = inputImage
+        
+        //ずっと回す
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([self.faceRequest])
+            } catch {
+                print(error)
+            }
+        }
+        
+        //イベント発生中であっても、一定時間は同じ処理を実行しない
+        //        let throttleAction = DispatchQueue.global().throttle(delay: .milliseconds(1000))
+        //         throttleAction { [unowned self] in
+        //            do {
+        //                try handler.perform([self.faceRequest])
+        //            } catch {
+        //                print(error)
+        //            }
+        //
+        //            let now = NSDate()
+        //            let formatter = DateFormatter()
+        //            formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        //            let str = formatter.string(from: now as Date)
+        //            print(str)
+        //        }
+        
+    }
+}
+
+//MARK: - Face detection
+
+extension ViewController {
+    
+    func vnRequestHandler(request: VNRequest, error: Error?) {
+        self.currentFaceNum = 1
+        
+        if let e = error {
+            print(e)
+            return
+        }
+        guard
+            let req = request as? VNDetectFaceRectanglesRequest,
+            let faces = req.results as? [VNFaceObservation],
+            let centerFace = faces.sorted(by: { (a, b) -> Bool in distanceToCenter(face: a) < distanceToCenter(face: b) }).first else {
+                self.currentFaceNum = 0
+                updateTimer()
+                return
+        }
+        guard let image = inputImage else {
+            return
+        }
+        
+        updateTimer()
+        drawFaceRectOverlay(image: image, face: centerFace)
+        
+        guard let cgImage = getFaceCGImage(image: image, face: centerFace) else {
+            return
+        }
+        
+        showPreview(cgImage: cgImage)
+        predicate(cgImage: cgImage)
+    }
+    
+    private func updateTimer(){
+        if(self.currentFaceNum != self.previousFaceNum){
+            if(self.currentFaceNum > 0){
+                //appear
+                //timerを生成する.
+                self.timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.onUpdate(timer:)), userInfo: nil, repeats: true)
+                print("face appear")
+            }else{
+                //disappear
+                //timerを破棄する.
+                self.timer.invalidate()
+                self.cnt = 0
+                print("face disappear")
+            }
+            self.previousFaceNum = self.currentFaceNum
+        }
+    }
+    
+    private func distanceToCenter(face: VNFaceObservation) -> CGFloat {
+        let x = face.boundingBox.origin.x + face.boundingBox.size.width / 2
+        let y = face.boundingBox.origin.y + face.boundingBox.size.height / 2
+        let pos = CGPoint(x: x, y: y)
+        let viewPos = CGPoint(x: 0.5, y: 0.5)
+        let distance = sqrt(pow(pos.x - viewPos.x, 2) + pow(pos.y - viewPos.y, 2))
+        return distance
+    }
+    
+    //device = 1920,1080 (imagesize/videoDim)
+    //image  = 736, 414
+    //screen = 414, 736
+    //
+    private func drawFaceRectOverlay(image: CIImage, face: VNFaceObservation) {
+        guard let viewSize = overlayViewSize else {
+            return
+        }
+        
+        var boxes:[CGRect] = []
+        
+        let box = face.boundingBox.scaledForOverlay(to: viewSize)
+        boxes.append(box)
+        
+        DispatchQueue.main.async {
+            self.overlayView.boxes = boxes
+            self.overlayView.setNeedsDisplay()
+            
+            self.facePos.text = String(format: "%.01f", Float(box.midX)) + ", " + String(format: "%.01f", Float(box.midY))
+        }
+    }
+    
+    private func getFaceCGImage(image: CIImage, face: VNFaceObservation) -> CGImage? {
+        let imageSize = image.extent.size
+        
+        let box = face.boundingBox.scaledForCropping(to: imageSize)
+        guard image.extent.contains(box) else {
+            return nil
+        }
+        let size = CGFloat(inputSize)
+        
+        let transform = CGAffineTransform(
+            scaleX: size / box.size.width,
+            y: size / box.size.height
+        )
+        let faceImage = image.cropped(to: box).transformed(by: transform)
+        
+        let ctx = CIContext()
+        guard let cgImage = ctx.createCGImage(faceImage, from: faceImage.extent) else {
+            assertionFailure()
+            return nil
+        }
+        return cgImage
+    }
+    
+    private func showPreview(cgImage:CGImage) {
+        let uiImage = UIImage(cgImage: cgImage)
+        DispatchQueue.main.async {
+            self.facePreview.image = uiImage
+        }
+    }
+}
+
+//MARK: - Predicate
+
+extension ViewController {
+    
+    fileprivate func predicate(cgImage: CGImage) {
+        let image = CIImage(cgImage: cgImage)
+        
+        //        print(image)
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            let model = try VNCoreMLModel(for: self.momomind.model)
+            let req = VNCoreMLRequest(model: model, completionHandler: self.handleClassification)
+            try handler.perform([req])
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func handleClassification(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNClassificationObservation]
+            else { fatalError("unexpected result type from VNCoreMLRequest") }
+        
+        DispatchQueue.main.async {
+            for ob in observations {
+                switch ob.identifier {
+                case "0-15male": self.updateLabel(idx: 0, ob: ob)
+                case "0-15female": self.updateLabel(idx: 1, ob: ob)
+                case "16-29male": self.updateLabel(idx: 2, ob: ob)
+                case "16-29female": self.updateLabel(idx: 3, ob: ob)
+                case "30-49male": self.updateLabel(idx: 4, ob: ob)
+                case "30-49female": self.updateLabel(idx: 5, ob: ob)
+                case "50-male": self.updateLabel(idx: 6, ob: ob)
+                case "50-female": self.updateLabel(idx: 7, ob: ob)
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    private func updateLabel(idx: Int, ob: VNClassificationObservation) {
+        let label = probabilityLabels.filter{ $0.tag == idx }.first
+        let progress = progressViews.filter{ $0.tag == idx }.first
+        
+        let per = Int(ob.confidence * 100)
+        label?.text = "\(per)%"
+        progress?.progress = ob.confidence
+    }
+}
+
+extension CGRect {
+    func scaledForOverlay(to size: CGSize) -> CGRect {
+        
+        let posx :CGFloat
+        
+        if(ViewController.isFrontCamera == true){
+            posx = (1.0 - self.origin.x - self.size.width) * size.width
+        }else{
+            posx = self.origin.x * size.width
+        }
+        
+        return CGRect(
+            x: posx,
+            y: (1.0 - self.origin.y - self.size.height) * size.height,
+            width: (self.size.width * size.width),
+            height: (self.size.height * size.height)
+        )
+    }
+    
+    func scaledForCropping(to size: CGSize) -> CGRect {
+        return CGRect(
+            x: self.origin.x * size.width,
+            y: self.origin.y * size.height,
+            width: (self.size.width * size.width),
+            height: (self.size.height * size.height)
+        )
+    }
+}
+
